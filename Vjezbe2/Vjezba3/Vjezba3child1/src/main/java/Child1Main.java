d3b09cb49231dae10fc10772be9d56f3750c6b7a
Simon Šporer <ssporer@gmail.com>
2017-10-25 10:41:56 +0200

    vjezbe 2

---
+++ Vjezbe2/Vjezba3/Vjezba3child1/src/main/java/Child1Main.java
@@ -0,0 +1,8 @@
+public class Child1Main {
+
+    private static final String GREETINGS = "Vjezba 3";
+
+    public static String getGREETINGS() {
+        return GREETINGS;
+    }
+}
---
+++ Vjezbe2/Vjezba3/Vjezba3child1/src/test/java/Test1.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Test1 {
+
+    @Test
+    public void test() {
+        assertEquals("Vjezba 3", Child1Main.getGREETINGS());
+    }
+
+}
