3c3ffac65ee75c2de26aebe3cbbcfd1c475eca50
zelbar <zeljko.baranek@gmail.com>
2017-10-30 18:48:04 +0100

    Add ZelBar component implementation
    Adds the ZelBar component into hr.fer.java.vjezbe.zadatak4.component package that implements the abstract CoreComponent and implements the setStudentName returning the student's name.

---
+++ Vjezbe03/zadatak4/src/main/java/hr/fer/java/vjezbe/zadatak4/component/ZelBarComponent.java
@@ -0,0 +1,13 @@
+package hr.fer.java.vjezbe.zadatak4.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ZelBarComponent extends CoreComponent {
+
+	@Override
+	protected String setStudentName() {
+		return "Željko";
+	}
+
+}
