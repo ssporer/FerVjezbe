626aeceedce12b70693876dea17cd0dcaf68084b
pere_mikulic <petarmikulic94@gmail.com>
2017-10-30 19:37:53 +0100

    PereMikulicRest

--- Vjezbe03/zadatak4/src/main/java/hr/fer/java/vjezbe/zadatak4/component/PereMikulicComponent.java
+++ Vjezbe03/zadatak4/src/main/java/hr/fer/java/vjezbe/zadatak4/component/PereMikulicComponent.java
@@ -6,10 +6,16 @@ import hr.fer.java.vjezbe.zadatak4.model.enums.Students;
 
 @Component
 public class PereMikulicComponent extends CoreComponent{
+	
+	private static final String MY_NAME=Students.PERE_MIKULIC.toString();
 
 	@Override
 	protected String setStudentName() {
-		return Students.PERE_MIKULIC.toString();
+		return MY_NAME;
+	}
+	
+	public String getMyName() {
+		return MY_NAME;
 	}
 
 }
