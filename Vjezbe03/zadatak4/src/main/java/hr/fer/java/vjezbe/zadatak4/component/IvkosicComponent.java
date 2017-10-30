24780bd37a3113a30a97b245127fefe1dc3a47f0
iivkosic <ivan.ivkosic@fer.hr>
2017-10-30 18:44:36 +0100

    fulao sam

---
+++ Vjezbe03/zadatak4/src/main/java/hr/fer/java/vjezbe/zadatak4/component/IvkosicComponent.java
@@ -0,0 +1,14 @@
+package hr.fer.java.vjezbe.zadatak4.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class IvkosicComponent extends CoreComponent {
+
+	@Override
+	protected String setStudentName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
