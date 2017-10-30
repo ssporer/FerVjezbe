fe17feaa785186d01f0e6f87e10e43651122c89a
pere_mikulic <petarmikulic94@gmail.com>
2017-10-30 19:10:40 +0100

    Pere enum

--- Vjezbe03/zadatak4/src/main/java/hr/fer/java/vjezbe/zadatak4/component/PereMikulicComponent.java
+++ Vjezbe03/zadatak4/src/main/java/hr/fer/java/vjezbe/zadatak4/component/PereMikulicComponent.java
@@ -2,12 +2,14 @@ package hr.fer.java.vjezbe.zadatak4.component;
 
 import org.springframework.stereotype.Component;
 
+import hr.fer.java.vjezbe.zadatak4.model.enums.Students;
+
 @Component
 public class PereMikulicComponent extends CoreComponent{
 
 	@Override
 	protected String setStudentName() {
-		return "Pere Mikulić";
+		return Students.PERE_MIKULIC.toString();
 	}
 
 }
