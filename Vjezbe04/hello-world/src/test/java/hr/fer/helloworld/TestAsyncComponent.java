ec53f4004e1961e017be92a2003e26a83a48770b
Matej Jelusic <matej.jelusic@altima.hr>
2017-11-06 19:59:38 +0100

    add test - fixed

--- Vjezbe04/hello-world/src/test/java/hr/fer/helloworld/TestAsyncComponent.java
+++ Vjezbe04/hello-world/src/test/java/hr/fer/helloworld/TestAsyncComponent.java
@@ -1,24 +1,23 @@
+package hr.fer.helloworld;
+
 import hr.fer.helloworld.component.AsyncComponent;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Configuration
 public class TestAsyncComponent {
+
     @Autowired
-    private AsyncComponent asyncComponent;
-
-
+    AsyncComponent asyncComponent;
 
     @Test
     public void testAsyncComponent(){
         for(int i = 0; i<10; i++){
-
+            asyncComponent.asyncMethodWithConfiguredExecutor(i);
         }
     }
 }
