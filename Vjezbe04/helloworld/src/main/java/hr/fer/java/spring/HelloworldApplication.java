954889f18bb650385cc602a56c3538f8744dd5df
Mislav <mislav.markovic@fer.hr>
2017-11-06 19:08:58 +0100

    Scheduled Hello World logger
    Signed-off-by: Mislav <mislav.markovic@fer.hr>

--- Vjezbe04/helloworld/src/main/java/hr/fer/java/spring/HelloworldApplication.java
+++ Vjezbe04/helloworld/src/main/java/hr/fer/java/spring/HelloworldApplication.java
@@ -2,12 +2,14 @@ package hr.fer.java.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
-		System.out.println("Hello World!");
+		//System.out.println("Hello World!");
 	}
 }
---
+++ Vjezbe04/helloworld/src/main/java/hr/fer/java/spring/component/ScheduledTask.java
@@ -0,0 +1,19 @@
+package hr.fer.java.spring.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Mislav on 6.11.2017..
+ */
+@Component
+public class ScheduledTask {
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTask.class);
+
+    @Scheduled(fixedRate = 2000)
+    public void sayHello(){
+        logger.info("Hello World!");
+    }
+}
