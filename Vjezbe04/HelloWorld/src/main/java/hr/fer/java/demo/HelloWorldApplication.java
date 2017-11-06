562fddebbac71510f1e4f758fb228a146c66a41b
Dan Ambrošić <dan.ambrosic@fer.hr>
2017-11-06 19:36:53 +0100

    async task

--- Vjezbe04/HelloWorld/src/main/java/hr/fer/java/demo/HelloWorldApplication.java
+++ Vjezbe04/HelloWorld/src/main/java/hr/fer/java/demo/HelloWorldApplication.java
@@ -2,13 +2,30 @@ package hr.fer.java.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
+		SpringApplication.run(HelloWorldApplication.class, args).close();
+	}
+
+	@Bean
+	public Executor asyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(4);
+		executor.setMaxPoolSize(4);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("HelloWorld");
+		executor.initialize();
+		return executor;
 	}
 }
