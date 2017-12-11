53cb526d17f4dfadb1b69607db5fcb5d97a11dfb
Simon Šporer <simon.sporer@serengetitech.com>
2017-12-11 15:22:51 +0100

    zadatak7_rjesenje

--- Vjezbe07/Zadatak7/src/main/java/hr/fer/java/vjezbe/zadatak7/domain/LibrarianDao.java
+++ Vjezbe07/Zadatak7/src/main/java/hr/fer/java/vjezbe/zadatak7/domain/LibrarianDao.java
@@ -1,6 +1,7 @@
 package hr.fer.java.vjezbe.zadatak7.domain;
 
 import hr.fer.java.vjezbe.zadatak7.model.LibrarianDto;
+import org.springframework.security.core.Authentication;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface LibrarianDao {
     List<LibrarianDto> findAll();
 
     LibrarianDto findByUsername(String username);
+
+    Authentication authenticateUser(Authentication authentication);
 }
